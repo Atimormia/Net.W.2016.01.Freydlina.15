2c509aa5524265e5605faf6202ca8b7db38f7c44
atimormia <atimormia@gmail.com>
2016-12-01 15:10:04 +0300

    Task2

---
+++ Net.W.2016.Freydlina.15/Task2.Tests/BinarySearchTreeTests.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using NUnit.Framework;
+using Task1;
+
+namespace Task2.Tests
+{
+    [TestFixture]
+    public class BinarySearchTreeTests
+    {
+        #region BinarySearchTreeTests for System.Int32
+        public class CustomIntComparer : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return y.CompareTo(x);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForGetPreOrderEnumeratorInts
+        {
+            get
+            {
+                IEnumerable<int> ints = new[] {7, 3, 10, 2, 5, 12, 1, 11, 15};
+                IEnumerable<int> intResult = new[] { 7,3,2,1,5,10,12,11,15 };
+                yield return new TestCaseData(ints, null).Returns(intResult);
+
+                intResult = new[] { 7,10,12,15,11,3,5,2,1};
+                yield return new TestCaseData(ints, new CustomIntComparer()).Returns(intResult);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGetPreOrderEnumeratorInts))]
+        public IEnumerable<int> TestGetPreOrderEnumeratorInts(int[] items,IComparer<int> comparer)
+        {
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(items,comparer);
+            IEnumerator<int> actual = tree.GetPreorderEnumerator();
+            
+            while (actual.MoveNext())
+            {
+                Console.Write(actual.Current+" ");
+                yield return actual.Current;
+            }
+        }
+        #endregion
+
+        #region BinarySearchTreeTests for System.String
+
+        public class CustomStringComparer : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                return string.Compare(y, x, false, CultureInfo.InvariantCulture);
+            }
+        }
+        
+        public static IEnumerable<TestCaseData> TestCasesForGetPreOrderEnumeratorStrings
+        {
+            get
+            {
+                IEnumerable<string> strings = new[] { "7", "3", "10", "2", "5", "12", "1", "11", "15" };
+                IEnumerable<string> stringResult = new[] { "7", "3", "10", "1", "2", "12", "11", "15", "5" };
+                yield return new TestCaseData(strings,null).Returns(stringResult);
+
+                stringResult = new[] { "7", "3", "5", "10", "2", "12", "15", "11", "1" };
+                yield return new TestCaseData(strings, new CustomStringComparer()).Returns(stringResult);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGetPreOrderEnumeratorStrings))]
+        public IEnumerable<string> TestGetPreOrderEnumeratorStrings(string[] items, IComparer<string> comparer)
+        {
+            BinarySearchTree<string> tree = new BinarySearchTree<string>(items,comparer);
+            IEnumerator<string> actual = tree.GetPreorderEnumerator();
+
+            while (actual.MoveNext())
+            {
+                Console.Write(actual.Current + " ");
+                yield return actual.Current;
+            }
+        }
+        #endregion
+
+        #region BinarySearchTreeTests for Book
+        public class CustomBookComparer : IComparer<Book>
+        {
+            public int Compare(Book x, Book y)
+            {
+                return x.CompareTo(y, (b1, b2) => b1.PublishingYear.CompareTo(b2.PublishingYear));
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForGetPreOrderEnumeratorBooks
+        {
+            get
+            {
+                IEnumerable<Book> books = new[] 
+                {
+                    new Book(".NET Core - .NET Goes Cross - Platform with.NET Core", "Microsoft", 2016,
+                        "Carter, Phillip", "Knezevic, Zlatko"),
+                    new Book("CrossNet", "Codeplex.com", 2012),
+                    new Book("Announcing.NET Framework 4.6.2", "Microsoft", 2016, "Haffner, Stacey"),
+                    new Book("Understanding .NET Just-In-Time Compilation", "Telerik"),
+                    new Book("Understanding Garbage Collection in .NET"),
+                };
+                IEnumerable<Book> bookResult = new[]
+                {
+                    new Book(".NET Core - .NET Goes Cross - Platform with.NET Core", "Microsoft", 2016,
+                        "Carter, Phillip", "Knezevic, Zlatko"),
+                    new Book("CrossNet", "Codeplex.com", 2012),
+                    new Book("Understanding .NET Just-In-Time Compilation", "Telerik"),
+                    new Book("Understanding Garbage Collection in .NET"),
+                    new Book("Announcing.NET Framework 4.6.2", "Microsoft", 2016, "Haffner, Stacey"),
+                };
+                yield return new TestCaseData(books, null).Returns(bookResult);
+
+                bookResult = new[] 
+                {
+                    new Book("Announcing.NET Framework 4.6.2", "Microsoft", 2016, "Haffner, Stacey"),
+                    new Book(".NET Core - .NET Goes Cross - Platform with.NET Core", "Microsoft", 2016,
+                        "Carter, Phillip", "Knezevic, Zlatko"),
+                    new Book("Understanding .NET Just-In-Time Compilation", "Telerik"),
+                    new Book("Understanding Garbage Collection in .NET"),
+                    new Book("CrossNet", "Codeplex.com", 2012)
+                };
+                yield return new TestCaseData(books, new CustomStringComparer()).Returns(bookResult);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForGetPreOrderEnumeratorBooks))]
+        public IEnumerable<Book> TestGetPreOrderEnumeratorBooks(Book[] items, IComparer<Book> comparer)
+        {
+            BinarySearchTree<Book> tree = new BinarySearchTree<Book>(items, comparer);
+            IEnumerator<Book> actual = tree.GetPreorderEnumerator();
+
+            while (actual.MoveNext())
+            {
+                Console.Write(actual.Current + " ");
+                yield return actual.Current;
+            }
+        }
+        #endregion
+
+        #region BinarySearchTreeTests for struct Point
+        public struct Point
+        {
+            private int x;
+            private int y;
+            public int X => x;
+            public int Y => y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        public class PointComparer : IComparer<Point>
+        {
+            public int Compare(Point x, Point y)
+            {
+                int res = x.X.CompareTo(y.X);
+                if (res == 0)
+                    res = x.Y.CompareTo(y.Y);
+                return res;
+            }
+        }
+
+        public static IEnumerable<TestCaseData> GetPreorderEnumeratorPointTestData
+        {
+            get
+            {
+                var items = new[]
+                {
+                    new Point(20, 20), new Point(10, 10), new Point(30, 30), new Point(5, 5), new Point(15, 15),
+                    new Point(35, 35), new Point(3, 3), new Point(8, 8), new Point(33, 33)
+                };
+                var result = new[]
+                {
+                    new Point(20, 20), new Point(10, 10), new Point(5, 5), new Point(3, 3), new Point(8, 8),
+                    new Point(15, 15), new Point(30, 30), new Point(35, 35), new Point(33, 33)
+                };
+                yield return new TestCaseData(items, new PointComparer()).Returns(result);
+                
+            }
+        }
+
+        [Test,TestCaseSource(nameof(GetPreorderEnumeratorPointTestData))]
+        public IEnumerable<Point> TestGetPreOrderEnumeratorPoints(Point[] items, IComparer<Point> comparer)
+        {
+            BinarySearchTree<Point> tree = new BinarySearchTree<Point>(items, comparer);
+            IEnumerator<Point> actual = tree.GetPreorderEnumerator();
+            
+            while (actual.MoveNext())
+            {
+                yield return actual.Current;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Net.W.2016.Freydlina.15/Task2/BinarySearchTree.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class BinarySearchTree<T>:IEnumerable<T>
+    {
+        private Node<T> top;
+        private readonly IComparer<T> comparer;
+
+        public BinarySearchTree(IComparer<T> comparer = null)
+        {
+            top = null;
+            this.comparer = comparer ?? Comparer<T>.Default;
+        }
+
+        public BinarySearchTree(IEnumerable<T> items, IComparer<T> comparer = null) : this(comparer)
+        {
+            if (ReferenceEquals(items, null))
+                throw new ArgumentNullException(nameof(items));
+            foreach (var item in items)
+                Add(item);
+        }
+
+        public bool IsEmpty() => top == null;
+
+        public bool Add(T value)
+        {
+            if (IsEmpty())
+            {
+                this.top = new Node<T>(value);
+                return true;
+            }
+            Node<T> current = this.top;
+            bool added = false;
+            do
+            {
+                if (comparer.Compare(value, current.Value) > 0)
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = new Node<T>(value);
+                        added = true;
+                    }
+                    current = current.Right;
+                }
+                else
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = new Node<T>(value);
+                        added = true;
+                    }
+                    current = current.Left;
+                }
+            } while (!added);
+            return true;
+        }
+
+        public IEnumerator<T> GetPreorderEnumerator()
+        {
+            Node<T> current = this.top;
+            Stack<Node<T>> stack = new Stack<Node<T>>();
+            while (current != null || stack.Count != 0)
+            {
+                if (stack.Count != 0) current = stack.Pop();
+                while (current != null)
+                {
+                    yield return current.Value;
+                    if (current.Right != null) stack.Push(current.Right);
+                    current = current.Left;
+                }
+            }
+        }
+
+        public IEnumerator<T> GetInorderEnumerator()
+        {
+            Node<T> current = this.top;
+            Stack<Node<T>> stack = new Stack<Node<T>>();
+            while (current != null || stack.Count != 0)
+            {
+                if (stack.Count != 0)
+                {
+                    current = stack.Pop();
+                    yield return current.Value;
+                    current = current.Right;
+                }
+                while (current != null)
+                {
+                    stack.Push(current);
+                    current = current.Left;
+                }
+            }
+        }
+
+        public IEnumerator<T> GetPostorderEnumerator()
+        {
+            Node<T> current = top;
+            Stack<Node<T>> stack = new Stack<Node<T>>();
+            while (current != null || stack.Count != 0)
+            {
+                if (stack.Count != 0)
+                {
+                    current = stack.Pop();
+                    if (stack.Count != 0 && current.Right == stack.Last())
+                    {
+                        current = stack.Pop();
+                    }
+                    else
+                    {
+                        yield return current.Value;
+                        current = null;
+                    }
+                }
+                while (current != null)
+                {
+                    stack.Push(current);
+                    if (current.Right != null)
+                    {
+                        stack.Push(current.Right);
+                        stack.Push(current);
+                    }
+                    current = current.Left;
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GetPreorderEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
